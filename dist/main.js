48871299afe86983bc19f032a2949d2be75c1439
Ti Jhae <me@gmail.com>
2020-04-25 18:18:55 +0000

    Done with Home page for mobile

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".menu-burger");
 const menuNav = document.querySelector(".menu-nav");
 const navContainer = document.querySelector(".menu-func");
 const btnLines = document.querySelectorAll(".btn-line");
+const main = document.querySelector("main");
 
 let showMenu = true;
 
@@ -9,10 +10,12 @@ btn.addEventListener("click", () => {
   if (showMenu) {
     btn.classList.add("rotate");
     menuNav.classList.add("show");
+    main.classList.add("fade");
     navContainer.classList.add("show");
     btnLines.forEach((line) => line.classList.add("show"));
     showMenu = false;
   } else {
+    main.classList.remove("fade");
     btn.classList.remove("rotate");
     menuNav.classList.remove("show");
     navContainer.classList.remove("show");
