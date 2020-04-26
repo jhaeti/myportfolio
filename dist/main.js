a4e366c98147cb40b1dec3592f4eac2bf460a7f9
Ti Jhae <me@gmail.com>
2020-04-26 12:41:40 +0000

    Done with homepage

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".menu-burger");
 const menuNav = document.querySelector(".menu-nav");
 const navContainer = document.querySelector(".menu-func");
 const btnLines = document.querySelectorAll(".btn-line");
+const navItems = document.querySelectorAll(".nav-items");
 const main = document.querySelector("main");
 
 let showMenu = true;
@@ -10,16 +11,16 @@ btn.addEventListener("click", () => {
   if (showMenu) {
     btn.classList.add("rotate");
     menuNav.classList.add("show");
-    main.classList.add("fade");
     navContainer.classList.add("show");
     btnLines.forEach((line) => line.classList.add("show"));
+    navItems.forEach((navItem) => navItem.classList.add("show"));
     showMenu = false;
   } else {
-    main.classList.remove("fade");
     btn.classList.remove("rotate");
     menuNav.classList.remove("show");
     navContainer.classList.remove("show");
     btnLines.forEach((line) => line.classList.remove("show"));
+    navItems.forEach((navItem) => navItem.classList.remove("show"));
     showMenu = true;
   }
 });
